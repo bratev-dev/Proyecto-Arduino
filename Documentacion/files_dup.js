d0e7f629ce95bed05c722c7af21cb70f505387b9
Brayan Gomes <124471330+bratev-dev@users.noreply.github.com>
2024-11-28 11:03:37 -0500

    Add files via upload

---
+++ Documentacion/files_dup.js
@@ -0,0 +1,4 @@
+var files_dup =
+[
+    [ "FinalMaquina.cpp", "_final_maquina_8cpp.html", "_final_maquina_8cpp" ]
+];
---
+++ Documentacion/topics.js
@@ -0,0 +1,5 @@
+var topics =
+[
+    [ "Definiciones de Frecuencias Musicales", "group___notas___musicales.html", null ],
+    [ "Configuracion de Pines de Hardware", "group___configuracion___pines.html", null ]
+];
